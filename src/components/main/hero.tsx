be875a3fc2e1d4646560233cb26d2eed9b4cdb9f
Abhishek Kumar <ak3037308@gmail.com>
2024-09-17 17:13:13 +0530

    added  the autosuggest input into the homepage as seperate component inside the hompage file for full height

--- src/components/main/hero.tsx
+++ src/components/main/hero.tsx
@@ -2,22 +2,10 @@
 
 import Image from "next/image";
 import React from "react";
-import SearchBar from "./autoSuggest";
+
 
 export const Hero = () => {
-  const customers = [
-    'Adam', 'Bella', 'Charlie', 'David', 'Fiona', 'George', 'Kelly',
-    'Samantha', 'Harry', 'Olivia', 'Isabella', 'Mason', 'Sophia', 'James', 
-    'Ava', 'Liam', 'Ethan', 'Noah', 'Emma', 'Michael', 'Lucas', 'Amelia',
-    'Alexander', 'Mia', 'Benjamin', 'Charlotte', 'Jack', 'Lily', 'Daniel',
-    'Ella', 'Henry', 'Grace', 'Matthew', 'Zoe', 'Owen', 'Scarlett', 
-    'Ryan', 'Hazel', 'Gabriel', 'Aria', 'William', 'Chloe', 'Sebastian'
-  ];
-  const handleSelect = (value: string) => {
-   console.log("Selected value: ", value);
-  };
-
-
+ 
   return (
     <div className="relative overflow-hidden  h-[40vh] md:min-h-[70vh] ">
       <div className="w-full flex flex-col md:flex-row justify-between items-center relative">
@@ -40,12 +28,12 @@ export const Hero = () => {
       </div>
 
       {/* Main Content Div */}
-      <div className="max-h-[400px] flex flex-col md:flex-row gap-3 w-full items-center justify-start md:justify-center h-[50vh] md:h-screen z-10  md:-mt-28 px-4 overflow-visible">
+      {/* <div className="max-h-[400px] flex flex-col md:flex-row gap-3 w-full items-center justify-start md:justify-center h-[50vh] md:h-screen z-10  md:-mt-28 px-4 overflow-visible">
         <div className="flex flex-col md:flex-row gap-2 ">
        <SearchBar  suggestions={customers}           onSelect={(value) => handleSelect(value)} />
         </div>
         
-      </div>
+      </div> */}
     </div>
   );
 };
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,9 +1,29 @@
+'use client'
+
 import { CyberSecurityServices, Hero, SecondaryServices, Testimonials } from "../components";
+import SearchBar from "../components/main/autoSuggest";
 
 export const HomePage = () =>{
+  const customers = [
+    'Adam', 'Bella', 'Charlie', 'David', 'Fiona', 'George', 'Kelly',
+    'Samantha', 'Harry', 'Olivia', 'Isabella', 'Mason', 'Sophia', 'James', 
+    'Ava', 'Liam', 'Ethan', 'Noah', 'Emma', 'Michael', 'Lucas', 'Amelia',
+    'Alexander', 'Mia', 'Benjamin', 'Charlotte', 'Jack', 'Lily', 'Daniel',
+    'Ella', 'Henry', 'Grace', 'Matthew', 'Zoe', 'Owen', 'Scarlett', 
+    'Ryan', 'Hazel', 'Gabriel', 'Aria', 'William', 'Chloe', 'Sebastian'
+  ];
+  const handleSelect = (value: string) => {
+   console.log("Selected value: ", value);
+  };
   return (
     <div>
 <Hero/>
+<div className="absolute top-24 md:top-1/2 flex flex-col md:flex-row gap-3 w-full items-center justify-start md:justify-center  z-10  md:-mt-28 px-4 overflow-visible">
+        <div className="flex flex-col md:flex-row gap-2 ">
+       <SearchBar  suggestions={customers}           onSelect={(value) => handleSelect(value)} />
+        </div>
+        
+      </div>
 <main>
   <CyberSecurityServices/>
   <SecondaryServices/>
