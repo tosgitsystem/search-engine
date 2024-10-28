07f0746c89f08bfc179c89c43f83902a76921a4c
sourav <mail.tos.systems@gmail.com>
2024-10-28 21:37:18 +0000

    changed

--- src/services/search/index.ts
+++ src/services/search/index.ts
@@ -2,26 +2,26 @@ import { Api } from "../config"
 
 
 export const search = async (data:{type:string ,q:string,page?:number})  => {
-    return (await Api.post("search?type=" + data.type, data)).data
+    return (await Api.post("/search?type=" + data.type, data)).data
 } 
 
 export const autoComplete = async (data:{q:string})  => {
-    return (await Api.post("autocomplete", data)).data
+    return (await Api.post("/autocomplete", data)).data
 } 
 
 
 
 export const videoSearch = async (data:{q:string})  => {
-    return (await Api.post("videos", data)).data
+    return (await Api.post("/videos", data)).data
 } 
 
 
 export const imageSearch = async (data:{q:string})  => {
-    return (await Api.post("images", data)).data
+    return (await Api.post("/images", data)).data
 } 
 
 export const newsSearch = async (data:{q:string})  => {
-    return (await Api.post("news", data)).data
+    return (await Api.post("/news", data)).data
 } 
 
 
